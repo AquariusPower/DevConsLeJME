0fca5fb934abc2614c3531dc2f6657e27034c533
AquariusPower <AquariusPower@users.noreply.github.com>
2017-04-05 21:30:58 -0300

    .

--- bin.projectManagement/devConsConsistencyCheck.sh
+++ bin.projectManagement/devConsConsistencyCheck.sh
@@ -31,47 +31,103 @@ SECFUNCuniqueLock --waitbecomedaemon
 
 #cd ..
 
-IFS=$'\n' read -d '' -r -a astrFileList < <(find -iname "*.java")&&:
+#~ IFS=$'\n' read -d '' -r -a astrFileList < <(find -iname "*.java")&&:
+#~ 
+#~ function FUNCvalidate(){ # <strFile> <strPkg> <strRestrictedTo>
+	#~ local lstrFile="$1";shift
+	#~ local lstrPkg="$1";shift
+	#~ local lstrRestrictedTo="$1";shift
+	#~ 
+	#~ if egrep -q "package com[.]github[.]devconslejme[.]${lstrPkg};" "$lstrFile";then
+		#~ bMatched=true;
+		#~ if \
+			#~ egrep "import com[.]github[.]devconslejme[.]" "$lstrFile" |\
+				#~ egrep -v "import com[.]github[.]devconslejme[.](${lstrRestrictedTo})" \
+		#~ ;then
+			#~ bHasProblems=true
+			#~ echoc -p "Lacking cohesion: only from $lstrRestrictedTo"
+		#~ fi
+	#~ fi
+#~ }
+#~ 
+#~ for strFile in "${astrFileList[@]}";do
+	#~ bMatched=false;
+	#~ bHasProblems=false;
+	#~ 
+#~ #	SECFUNCdrawLine --stay --left " $strFile " #file info
+	#~ SECFUNCdrawLine --left " $strFile " #file info
+	#~ 
+	#~ # tests import all
+	#~ if [[ "$strFile" =~ [.]/Tests/.* ]];then
+		#~ continue
+	#~ fi
+	#~ 
+	#~ FUNCvalidate "$strFile" "misc" "misc"
+	#~ FUNCvalidate "$strFile" "extras" "extras"
+	#~ FUNCvalidate "$strFile" "devcons" "misc|gendiag|devcons"
+	#~ FUNCvalidate "$strFile" "gendiag" "misc|gendiag"
+	#~ 
+	#~ if ! $bMatched;then
+		#~ echoc -p "out of scope"
+	#~ fi
+	#~ 
+	#~ if $bHasProblems;then
+		#~ echoc --info --say "problems found"
+	#~ fi
+#~ done
 
-function FUNCvalidate(){ # <strFile> <strPkg> <strRestrictedTo>
-	local lstrFile="$1";shift
+strAllDeps="`secJavaDependencyList.sh |sort -u |grep ":com/github/devconslejme"`"
+
+#~ echoc --info "anything that shows below will be a problem..."
+
+function FUNCchk(){ # <package> <what packages beyond self can it access>
 	local lstrPkg="$1";shift
-	local lstrRestrictedTo="$1";shift
 	
-	if egrep -q "package com[.]github[.]devconslejme[.]${lstrPkg};" "$lstrFile";then
-		bMatched=true;
-		if \
-			egrep "import com[.]github[.]devconslejme[.]" "$lstrFile" |\
-				egrep -v "import com[.]github[.]devconslejme[.](${lstrRestrictedTo})" \
-		;then
-			bHasProblems=true
-			echoc -p "Lacking cohesion: only from $lstrRestrictedTo"
-		fi
+	local lastrOtherPkgs=()
+	if [[ -n "${1-}" ]];then
+		lastrOtherPkgs+=("${@-}")
 	fi
+	
+	lastrOtherPkgs+=($lstrPkg)
+	
+	echoc --info "Any pkg dep shown below will be a high coupling problem for: @{g}$lstrPkg @{r}${lastrOtherPkgs[@]}"
+	
+	local lstrOtherPkgs="`echo "${lastrOtherPkgs[@]}" |tr " " "|"`"
+	
+	# grep: before ':' is the full path for the class file being analised
+	if echo "$strAllDeps" |grep "/$lstrPkg/[^/]*:" |egrep -v ":(.*/($lstrOtherPkgs)/)";then
+		bProblemFound=true
+	fi
+	
+	return 0 #prevents last cmd no-problem error
 }
 
-for strFile in "${astrFileList[@]}";do
-	bMatched=false;
-	bHasProblems=false;
-	
-#	SECFUNCdrawLine --stay --left " $strFile " #file info
-	SECFUNCdrawLine --left " $strFile " #file info
-	
-	# tests import all
-	if [[ "$strFile" =~ [.]/Tests/.* ]];then
-		continue
-	fi
-	
-	FUNCvalidate "$strFile" "misc" "misc"
-	FUNCvalidate "$strFile" "extras" "extras"
-	FUNCvalidate "$strFile" "devcons" "misc|gendiag|devcons"
-	FUNCvalidate "$strFile" "gendiag" "misc|gendiag"
-	
-	if ! $bMatched;then
-		echoc -p "out of scope"
-	fi
-	
-	if $bHasProblems;then
-		echoc --info --say "problems found"
-	fi
-done
+bProblemFound=false
+
+# misc can only access misc
+#echo "$strAllDeps" |grep "/misc/[^/]*:" |egrep -v ":(.*/misc/)"&&:
+FUNCchk misc
+
+# misc/jme can only access misc or misc/jme
+#echo "$strAllDeps" |grep "/misc/jme/[^/]*:" |egrep -v ":(.*/misc/|.*/misc/jme/)"&&:
+FUNCchk misc/jme misc
+
+# misc/lemur can only access misc or misc/jme or misc/lemur
+#echo "$strAllDeps" |grep "/misc/lemur/[^/]*:" |egrep -v ":(.*/misc/|.*/misc/jme/|.*/misc/lemur/)"&&:
+FUNCchk misc/lemur misc/jme misc
+
+# gendiag can only access misc or misc/jme or misc/lemur or gendiag
+#echo "$strAllDeps" |grep "/gendiag/[^/]*:" |egrep -v ":(.*/misc/|.*/misc/jme/|.*/misc/lemur/|.*/gendiag/)"&&:
+FUNCchk gendiag misc/lemur misc/jme misc
+
+# devcons can only access misc or misc/jme or misc/lemur or gendiag or devcons
+#echo "$strAllDeps" |grep "/devcons/[^/]*:" |egrep -v ":(.*/misc/|.*/misc/jme/|.*/misc/lemur/|.*/gendiag/|.*/devcons/)"&&:
+FUNCchk devcons gendiag misc/lemur misc/jme misc
+
+# extras can only access extras
+#echo "$strAllDeps" |grep "/extras/[^/]*:" |egrep -v ":(.*/extras/)"&&:
+FUNCchk extras
+
+if $bProblemFound;then
+	echoc -p --info --say "problem found!"
+fi
