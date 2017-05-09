53e21baade55c7d4a8e214bc949bc261e1c765ed
AquariusPower <AquariusPower@users.noreply.github.com>
2017-05-08 21:36:19 -0300

    .

--- Tests/src/main/java/com/github/devconslejme/tests/temp/TestSimpleThings.java
+++ Tests/src/main/java/com/github/devconslejme/tests/temp/TestSimpleThings.java
@@ -4,11 +4,10 @@ import com.github.devconslejme.misc.StringI;
 
 public class TestSimpleThings {
 	public static void main(String[] args) {
+		tst1();
+	}
+	public static void tst1(){
 		System.out.println(StringI.i().convertToUniqueId(Long.MAX_VALUE));
 		System.out.println((Long.MAX_VALUE));
-//		System.out.println(">>>"+StringI.i().getNextUniqueId(""+(Long.MAX_VALUE-1)));
-//		BigInteger bi = new BigInteger(""+(Long.MAX_VALUE-1), 10);
-//		BigInteger bi2 = new BigInteger("0", 36);
-//		System.out.println(">>>"+(bi.add(bi2)).toString(36));
 	}
 }
